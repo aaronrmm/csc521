cf490e2f5554a59d04076b2e77ea7e9f11a75395
aaronrmm <aaronrmm@gmail.com>
2016-09-08 03:40:38 -0400

    Removed comments from OriginalForkExample

--- HelloProcessing/src/hw2section2/OriginalForkExample.java
+++ HelloProcessing/src/hw2section2/OriginalForkExample.java
@@ -1,44 +1,43 @@
 package hw2section2;
 
 
-//Demonstrating multithreading and thread synchronization in Java
+
 public class OriginalForkExample implements Runnable {
 
-	int i; // the ID of the thread, so we can control behavior
-	boolean busy; // the flag, Thread 1 will wait until Thread 0 is no longer busy before continuing
-	OriginalForkExample other; // reference to the other thread we will synchronize on. This is needed so we can control behavior.
+	int i;
+	boolean busy;
+	OriginalForkExample other;
 
-	// create the runnable object
+
 	public OriginalForkExample(int i, OriginalForkExample other) {
-		this.i = i; // set the thread ID (0 or 1)
-		if(i==0) { busy = true; } // set the busy flag so Thread 1 waits for Thread 0
+		this.i = i;
+		if(i==0) { busy = true; }
 		else { this.other = other; }
 	}
 
-	// synchronized method to test if thread is busy or not
-	public synchronized boolean isBusy() { return busy; } // What happens if this isn't synchronized? 
 
-	// run method needed by runnable interface
+	public synchronized boolean isBusy() { return busy; } 
+
+
 	public void run() {
-		if(i==0) { // 1st thread, sleep for a while, then notify threads waiting
+		if(i==0) {
 			try {
-				Thread.sleep(4000); // What happens if you put this sleep inside the synchronized block?
+				Thread.sleep(4000);
 				synchronized(this) {
-					notify(); // notify() will only notify threads waiting on *this* object;
+					notify();
 				}
-				Thread.sleep(4000); // What happens if you put this sleep inside the synchronized block?
+				Thread.sleep(4000);
 				synchronized(this) {
-					busy = false; // must synchronize while editing the flag
-					notify(); // notify() will only notify threads waiting on *this* object;
+					busy = false;
+					notify();
 				}
 			}
 			catch(InterruptedException tie) { tie.printStackTrace(); }
 		}
 		else {
-			while(other.isBusy()) { // check if other thread is still working
+			while(other.isBusy()) {
 				System.out.println("Waiting!");
-				// must sychnronize to wait on other object
-				try { synchronized(other) { other.wait(); } } // note we have synchronized on the object we are going to wait on
+				try { synchronized(other) { other.wait(); } }
 				catch(InterruptedException tie) { tie.printStackTrace(); }
 			}
 			System.out.println("Finished!");
