cbdd9fde9198f9dd7e71a904cf430e1cd1c721fa
aaronrmm <aaronrmm@gmail.com>
2016-09-18 13:41:17 -0400

    Added test for section 3

---
+++ HelloProcessing/src/tests/HW1Section3Test.java
@@ -0,0 +1,66 @@
+package tests;
+
+import hw1section3.Client;
+import hw1section3.Server;
+
+public class HW1Section3Test {
+
+	public static void main(String[]args){
+		new Thread(new Runnable(){
+			@Override
+			public void run(){
+				Server.main(new String[]{""});
+			}
+		}).start();
+		new Thread(new Runnable(){
+			@Override
+			public void run(){
+				Client.main(new String[]{"200","0"});
+			}
+		}).start();
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		new Thread(new Runnable(){
+			@Override
+			public void run(){
+				Client.main(new String[]{"400","0"});
+			}
+		}).start();
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		new Thread(new Runnable(){
+			@Override
+			public void run(){
+				Client.main(new String[]{"600","0"});
+			}
+		}).start();
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		new Thread(new Runnable(){
+			@Override
+			public void run(){
+				Client.main(new String[]{"0","200"});
+			}
+		}).start();
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		new Thread(new Runnable(){
+			@Override
+			public void run(){
+				Client.main(new String[]{"200","200"});
+			}
+		}).start();
+	}
+}
