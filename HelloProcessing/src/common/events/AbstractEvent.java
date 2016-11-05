d4100f5c9ffe16d6cd55df9fa0366796b9b640c4
aaronrmm <aaronrmm@gmail.com>
2016-11-05 16:37:51 -0400

    Gave events reference to the scene in which they were created

--- HelloProcessing/src/common/events/AbstractEvent.java
+++ HelloProcessing/src/common/events/AbstractEvent.java
@@ -2,6 +2,9 @@ package common.events;
 
 import java.io.Serializable;
 
+import game.Game;
+import game.Scene;
+
 public abstract class AbstractEvent implements Serializable{
 
 	/**
@@ -13,12 +16,14 @@ public abstract class AbstractEvent implements Serializable{
 	public long id;
 	public long clientId;
 	public static long id_gen = 0;
+	public transient Scene scene;
 	
 	@Override public String toString(){
 		return super.toString()+"eventID:"+id;
 	}
 	
 	public AbstractEvent(){
+		this.scene = Game.current_scene;
 		this.id = id_gen++;
 	}
 	
