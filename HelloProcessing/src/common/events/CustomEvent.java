9bb1dce3bc62a6706c4c2c5bd96b824053b44c25
aaronrmm <aaronrmm@gmail.com>
2016-11-05 15:34:46 -0400

    Removed Event class

--- HelloProcessing/src/common/events/CustomEvent.java
+++ HelloProcessing/src/common/events/CustomEvent.java
@@ -1,10 +1,16 @@
 package common.events;
 
-public class CustomEvent extends Event{
+public class CustomEvent extends AbstractEvent{
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void Handle() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
