13e3af9811361d9d3e6503bfd9a84fadc2032ce9
aaronrmm <aaronrmm@gmail.com>
2016-11-23 03:28:55 -0500

    Forgot to commit ScriptComponent changes

--- HelloProcessing/src/common/ScriptComponent.java
+++ HelloProcessing/src/common/ScriptComponent.java
@@ -10,21 +10,25 @@ public class ScriptComponent extends AbstractComponent{
 	private static final long serialVersionUID = 1L;
 
 	public String script_method_name;
+	public Object args;
 	
-	public ScriptComponent(String script_method_name, GameObject gameObject){
+	public ScriptComponent(String script_method_name, GameObject gameObject, Object args){
 		super(gameObject);
+		this.args = args;
 		this.script_method_name = script_method_name;
 		ScriptManager.addScriptComponent(this);
 	}
 
 	@Override
 	public void destroy() {
-		// TODO Auto-generated method stub
+		ScriptManager.removeScriptComponent(this);
 		
 	}
 	
 	public void update(){
-		ScriptManager.bindArgument("game_object", this.getGameObject());
-		ScriptManager.executeScript(this.script_method_name);
+		if(args==null)
+			ScriptManager.executeScript(this.script_method_name);
+		else
+			ScriptManager.executeScript(this.script_method_name, args);
 	}
 }
