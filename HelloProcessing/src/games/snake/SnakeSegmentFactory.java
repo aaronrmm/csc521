f6890cd7a77b54cdf402cc50f8a38ef502b4d5c2
aaronrmm <aaronrmm@gmail.com>
2016-11-28 20:40:04 -0500

    Removed unused implementation and methods in SnakeSegmentFactory

--- HelloProcessing/src/games/snake/SnakeSegmentFactory.java
+++ HelloProcessing/src/games/snake/SnakeSegmentFactory.java
@@ -6,36 +6,20 @@ import common.GameObject;
 import common.RenderableComponent;
 import common.RenderingEngine;
 import common.ScriptComponent;
-import games.platforms.PlayerObjectFactory;
 import physics.PhysicsComponent;
 import physics.PhysicsEngine;
 import physics.Rectangle;
 
-public class SnakeSegmentFactory extends PlayerObjectFactory{
+public class SnakeSegmentFactory {
 
 	PhysicsEngine physE;
 	RenderingEngine renderE;
 	
 	public SnakeSegmentFactory(PhysicsEngine physics, RenderingEngine rendering, EventManagementEngine eventE) {
-		super(physics, rendering, eventE);
 		this.physE = physics;
 		this.renderE = rendering;
 	}
-	
-	@Override
-	public GameObject create(int x, int y, long clientId){
-		GameObject segment = new GameObject(EntityClass.PLAYER);
-		PhysicsComponent physics = new PhysicsComponent(segment, new Rectangle(x,y, 30, 30), false, physE);
-		segment.add(physics);
-		this.physE.addDynamicObject(physics, x, y);
-		RenderableComponent renderable = new RenderableComponent(segment, physics, renderE, new int[]{100,255,0,255});
-		segment.add(renderable);
-		this.renderE.addObject(renderable);
-		segment.alive = true;
-		segment.networked = true;
-		return segment;
-		
-	}
+
 	public GameObject create(int x, int y, int width, int height, long clientId){
 		GameObject segment = new GameObject(EntityClass.PLAYER);
 		PhysicsComponent physics = new PhysicsComponent(segment, new Rectangle(x,y, width, height), false, physE);
